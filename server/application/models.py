8c5f243318c29e1471e222b62268a2aba626b058
kurrbanov <justnick1312@gmail.com>
2022-03-31 14:36:25 +0300

    create model laptop

--- server/application/models.py
+++ server/application/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Laptop(models.Model):
+    BRAND = (
+        ('Apple', 'Apple'),
+        ('Acer', ' Acer'),
+        ('Asus', 'Asus'),
+        ('HP', 'HP'),
+        ('Lenovo', 'Lenovo'),
+        ('Samsung', 'Samsung'),
+        ('Xiaomi', 'Xiaomi')
+    )
+
+    PROCESSOR = (
+        ('Intel', 'Intel'),
+        ('AMD', 'AMD'),
+        ('Apple', 'Apple'),
+        ('Эльбрус', 'Эльбрус'),
+        ('Байкал', 'Байкал')
+    )
+
+    name = models.CharField(max_length=255, blank=False)
+    brand = models.CharField(max_length=20, blank=False, choices=BRAND)
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    weight = models.FloatField()
+    display = models.DecimalField(max_digits=3, decimal_places=1)
+    ram = models.IntegerField()
+    processor = models.CharField(max_length=10, choices=PROCESSOR)
