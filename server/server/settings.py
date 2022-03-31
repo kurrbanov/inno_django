c4729b2147364444234f57eeb303dcf4426d1707
kurrbanov <justnick1312@gmail.com>
2022-03-31 14:08:25 +0300

    cofigure db

--- server/server/settings.py
+++ server/server/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'shop_user',
+        'PASSWORD': 'shop_user',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
